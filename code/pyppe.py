745b1d708aa456fa712b39ba4d612f3360113932
Venelin Valkov <venelin@curiousily.com>
2017-12-02 15:23:06 +0200

    Add initial web environment

---
+++ code/pyppe.py
@@ -0,0 +1,105 @@
+import asyncio
+from pyppeteer.launcher import launch
+from collections import Counter
+
+# async def main(browser):
+    # page = await browser.newPage()
+    # await page.goto('http://example.com')
+    # await page.screenshot({'path': 'example.png'})
+
+    # dimensions = await page.evaluate('''() => {
+        # return {
+            # width: document.documentElement.clientWidth,
+            # height: document.documentElement.clientHeight,
+            # deviceScaleFactor: window.devicePixelRatio,
+        # }
+    # }''')
+
+    # print(dimensions)
+
+    # await browser.close()
+    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+
+loop = asyncio.get_event_loop()
+browser = launch(headless=False)
+page = loop.run_until_complete(browser.newPage())
+loop.run_until_complete(page.goto('http://127.0.0.1:8080/'))
+# loop.run_until_complete(page.waitFor(3))
+
+
+# loop.run_until_complete(page.waitFor(2))
+
+# element = loop.run_until_complete(page.querySelector('.new-todo'))
+# element = loop.run_until_complete(page.querySelector('.new-todo'))
+# boundingBox = loop.run_until_complete(element.evaluate('''(element) => {
+    # const bBox = element.getBoundingClientRect()
+    # return {
+      # x: bBox.x,
+      # y: bBox.y,
+      # width: bBox.width,
+      # height: bBox.height
+    # }
+# }'''))
+# # boundingBox = loop.run_until_complete(element.evaluate('(element) => element.getX()'))
+# # boundingBox = loop.run_until_complete(page.evaluate("() => document.getElementsByClassName('items_list--item')[0].getBoundingClientRect().width"))
+# print(boundingBox)
+
+# loop.run_until_complete(element.tap())
+
+elements = loop.run_until_complete(page.evaluate('''
+    () => {
+        function fullPath(el){
+            var names = [];
+            while (el.parentNode){
+                if (el.id){
+                    names.unshift('#'+el.id);
+                    break;
+                }else{
+                    if (el==el.ownerDocument.documentElement) 
+                        names.unshift(el.tagName.toLowerCase());
+                    else{
+                        for (var c=1,e=el;e.previousElementSibling;e=e.previousElementSibling,c++);
+                        names.unshift(el.tagName.toLowerCase()+":nth-child("+c+")");
+                    }
+                    el=el.parentNode;
+                }
+            }
+            return names.join(" > ");
+        }
+
+        var nodes = document.querySelectorAll("*")
+        var elements = []
+        nodes.forEach(function(v, _, _) {elements.push(v)})
+        return elements.map((e) => {
+            var bBox = e.getBoundingClientRect()
+            return {
+                element: e.localName,
+                selector: fullPath(e),
+                x: bBox.x,
+                y: bBox.y,
+                width: bBox.width,
+                height: bBox.height
+            }
+        })
+    }
+'''))
+
+print(elements[0])
+
+
+coverage = loop.run_until_complete(page.evaluate('() => {return window.__coverage__}'))
+
+def file_coverage(statement_coverage):
+    s = list(statement_coverage.values())
+    cnt = Counter(s)
+    stmts = sum(cnt.values())
+    return (cnt[0], stmts)
+
+def total_coverage(coverage):
+    cov = [file_coverage(v['s']) for v in coverage.values()]
+
+    not_covered = sum(n for n, _ in cov)
+    all_stmts = sum(c for _, c in cov)
+    return not_covered / all_stmts
+
+print(total_coverage(coverage))
